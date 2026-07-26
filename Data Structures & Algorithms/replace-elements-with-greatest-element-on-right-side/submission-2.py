649187ff3c4b4906170346db1148fa60a0ac0a55
Jaskaran S. Panaich <jkpanaich@gmail.com>
2026-07-25 20:51:57 -0700

    Add: replace-elements-with-greatest-element-on-right-side - submission-2

---
+++ Data Structures & Algorithms/replace-elements-with-greatest-element-on-right-side/submission-2.py
@@ -0,0 +1,16 @@
+class Solution:
+    def replaceElements(self, arr: List[int]) -> List[int]:
+        max_value_so_far = -1 # Keeps track of the largest value seen as iterating over array
+
+        # Reversed array and starting from end of array to count greatest element         
+        for index in range(len(arr) - 1, -1, -1): # Range takes input start, stop, step
+            tmp = arr[index] # Assign the temporary variable to value
+            arr[index] = max_value_so_far # Assign the value to the max value found so far
+            max_value_so_far = max(tmp, max_value_so_far)
+        
+        return arr
+
+
+        
+             
+        
