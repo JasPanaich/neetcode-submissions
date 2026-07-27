79aef91001106e05e576ca8ce65132a435bdd9fb
Jaskaran S. Panaich <jkpanaich@gmail.com>
2026-07-27 16:32:19 -0700

    Add: concatenation-of-array - submission-1

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-1.py
@@ -0,0 +1,7 @@
+class Solution:
+    def getConcatenation(self, nums: List[int]) -> List[int]:
+        # Create new array double the capacity
+        ans = nums
+        nums = 2 * ans
+        
+        return nums
